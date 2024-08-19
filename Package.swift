a0184fd1a9915051ff85fa1ce6fc7a38386a9ea6
James Lane <jrlane4@icloud.com>
2024-08-18 21:51:18 -0600

    SPM retry

--- Package.swift
+++ Package.swift
@@ -1,25 +1,23 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "JLImageDownloader",
-    platforms: [
-        .iOS(.v13)
-    ],
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "JLImageDownloader",
-            targets: ["JLImageDownloader"]
-        ),
-    ],
-    dependencies: [
-        // Add any dependencies if your framework relies on other packages
+            targets: ["JLImageDownloader"]),
     ],
     targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "JLImageDownloader",
-            dependencies: [],
-            path: "JLImageDownloader",
-        )
+            name: "JLImageDownloader"),
+        .testTarget(
+            name: "JLImageDownloaderTests",
+            dependencies: ["JLImageDownloader"]),
     ]
 )
